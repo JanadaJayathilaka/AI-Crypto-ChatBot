ba39c54044ce234e9bf99534c6961c6e23c79b9d
janada jayathilaka <lakvindujayathilaka@gmail.com>
2025-05-31 09:53:40 +0530

    first commit

---
+++ chatBot/chatBot/src/main/java/com/jana/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.jana.controller;
+
+
+import com.jana.response.ApiResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    public ResponseEntity<ApiResponse> HomeController(){
+        return null;
+    }
+}
---
+++ chatBot/chatBot/src/main/java/com/jana/response/ApiResponse.java
@@ -0,0 +1,10 @@
+package com.jana.response;
+
+
+import lombok.Data;
+
+@Data
+public class ApiResponse {
+
+    private String message;
+}
